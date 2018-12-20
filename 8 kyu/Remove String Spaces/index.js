f1575fa2b4dfc2f8c5fb51c99598ce8476020543
Marcin Świrek <swirek.marcin@gmail.com>
2018-12-20 08:36:22 +0100

    Add 8 kyu kata

---
+++ 8 kyu/Remove String Spaces/index.js
@@ -0,0 +1,9 @@
+// Simple, remove the spaces from the string, then return the resultant string.
+
+// FUNDAMENTALSSTRINGSARRAYS
+
+// Solution
+
+function noSpace(x) {
+  return x.split(' ').join('');
+}
