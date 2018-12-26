a2094d4b1ebb6a1e4e24350fab6ea27cd5a5a72c
Marcin Świrek <swirek.marcin@gmail.com>
2018-12-26 22:55:00 +0100

    Add 8 kyu kata

---
+++ 8 kyu/String Templates - Bug Fixing #5/index.js
@@ -0,0 +1,10 @@
+// Oh no! Timmy hasn't followed instructions very carefully and forgot how to use the new String Template feature, Help Timmy with his string template so it works as he expects!
+
+// ◄PREVIOUS KATA NEXT KATA►
+// BUGSSTRINGSES2015BABEL
+
+//My solution
+
+function buildString(...template) {
+  return `I like ${template.join(', ')}!`;
+}
