f2a798729a8428f735c1f5538ede8dd18f245b74
Marcin Świrek <swirek.marcin@gmail.com>
2019-01-06 12:46:08 +0100

    Add 7 and 8kyu kata

---
+++ 8 kyu/Regular Ball Super Ball/index.js
@@ -0,0 +1,27 @@
+// Regular Ball Super Ball
+// Create a class Ball.
+
+// Ball objects should accept one argument for "ball type" when instantiated.
+
+// If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+
+// ball1 = new Ball();
+// ball2 = new Ball("super");
+
+// ball1.ballType     //=> "regular"
+// ball2.ballType     //=> "super"
+// FUNDAMENTALSCLASSESBASIC LANGUAGE FEATURESOBJECT-ORIENTED PROGRAMMINGOBJECTS
+
+//My solution
+
+const Ball = function(ballType = 'regular') {
+  this.ballType = ballType;
+};
+
+// ES6
+
+class Ball {
+  constructor(ballType = 'regular') {
+    this.ballType = ballType;
+  }
+}
