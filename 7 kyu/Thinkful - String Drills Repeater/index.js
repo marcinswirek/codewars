0f9afc85a587123ade0284ef7159c6a5a1723839
Marcin Świrek <swirek.marcin@gmail.com>
2019-02-02 22:40:26 +0100

    Add 7 kyu kata

---
+++ 7 kyu/Thinkful - String Drills Repeater/index.js
@@ -0,0 +1,12 @@
+// Write a class function named repeat() that takes two arguments (a string and a long integer), and returns a new string where the input string is repeated that many times.
+
+// Example:
+// Repeater.repeat("a", 5)
+// should return
+
+// "aaaaa"
+// FUNDAMENTALS
+
+//Solution
+
+const repeater = (string, n) => string.repeat(n);
