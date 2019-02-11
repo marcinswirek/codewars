91562b521bae55400be107f2afc958cee1537035
Marcin Świrek <swirek.marcin@gmail.com>
2019-02-11 22:43:10 +0100

    Add 8kyu kata

---
+++ 8 kyu/String repeat/index.js
@@ -0,0 +1,8 @@
+// Write a function called repeatStr which repeats the given string string exactly n times.
+
+// repeatStr(6, "I") // "IIIIII"
+// repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+// FUNDAMENTALS
+
+//Solution
+const repeatStr = (n, s) => s.repeat(n);
