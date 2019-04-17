c63aa9e72f4f7fb7274ad6cd6ba56154efe6bfef
Marcin Świrek <swirek.marcin@gmail.com>
2019-04-17 21:40:10 +0200

    Add 7kyu kata

---
+++ 7 kyu/Reverse a Number/index.js
@@ -0,0 +1,38 @@
+// Given a number, write a function to output its reverse digits. (e.g. given 123 the answer is 321)
+
+// Numbers should preserve their sign; i.e. a negative number should still be negative when reversed.
+
+// Examples
+//  123 ->  321
+// -456 -> -654
+// 1000 ->    1
+// PUZZLESINTERVIEW QUESTIONSNUMBERS
+
+//solution
+const reverseNumber = n =>
+  n >= 0
+    ? n
+        .toString()
+        .split('')
+        .reverse()
+        .join('') * 1
+    : n
+        .toString()
+        .split('')
+        .splice(1)
+        .concat('-')
+        .reverse()
+        .join('') * 1;
+
+//Other solutions
+function reverseNumber(n) {
+  let isNegative = n < 0;
+  let reverseAsString = Math.abs(n)
+    .toString()
+    .split('')
+    .reverse()
+    .join('');
+  let result = Number(reverseAsString);
+
+  return isNegative ? -result : result;
+}
