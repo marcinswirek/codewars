eedf8503ed862a8b3775ee1abb4f07f1f1872aa2
Marcin Świrek <swirek.marcin@gmail.com>
2019-07-24 23:02:54 +0200

    Ad 8kyu kata

---
+++ 8 kyu/Count Odd Numbers below n/index.js
@@ -0,0 +1,10 @@
+// Given a number n, return the number of positive odd numbers below n, EASY!
+
+// oddCount(7) //=> 3, i.e [1, 3, 5]
+// oddCount(15) //=> 7, i.e [1, 3, 5, 7, 9, 11, 13]
+// Expect large Inputs!
+
+// PUZZLESGAMESALGORITHMS
+
+//Solution
+const oddCount = n => (n % 2 ? (n / 2).toFixed() - 1 : n / 2);
